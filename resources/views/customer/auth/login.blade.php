611c773bc42048a51e4b9f5cc1f2e4395eda2e11
Manh Huy Vo <mhuyvo@nexigen.digital>
2023-10-19 18:53:03 +1100

    Styling changes

--- resources/views/customer/auth/login.blade.php
+++ resources/views/customer/auth/login.blade.php
@@ -6,8 +6,7 @@
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a href="#" class="flex flex-col mb-6 text-2xl font-semibold text-gray-900">
             <div class="flex items-center">
-                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-                Beeline Logisitcs
+                <img src="{{ asset('assets/images/logo_landscape.png') }}" class="h-12 mr-4" alt="FlowBite Logo" />
             </div>  
             <span class="text-[14px] italic text-right font-medium text-gray-500">CUSTOMER PORTAL</span>
         </a>
@@ -27,12 +26,12 @@
                         <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 " required="">
                     </div>
                     <div class="flex items-center justify-end">
-                        <a href="#" id="forgot_password_btn" class="text-sm font-medium text-blue-600 hover:underline ">Forgot password?</a>
+                        <a href="#" id="forgot_password_btn" class="text-sm font-medium text-blue-700 hover:underline ">Forgot password?</a>
                     </div>
                     <div class="flex items-center justify-center border-solid border-red-500 border-[2px] py-1 rounded-lg text-sm opacity-60 hidden" id="forgot_password_message">
                         <p class="text-center font-semibold text-red-500">Please contact us to manually reset your password.</p>
                     </div>
-                    <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                    <button type="submit" class="w-full text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
                 </form>
             </div>
         </div>

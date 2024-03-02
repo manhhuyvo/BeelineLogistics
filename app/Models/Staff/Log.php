d43f869e08377aa56c189deec4f40fb4326abd10
Manh Huy Vo <manhhuyvo@gmail.com>
2024-03-02 16:36:27 +1100

    Add models for user, staff, supplier, customer logs

---
+++ app/Models/Staff/Log.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\Staff;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Staff;
+use App\Models\User;
+
+class Log extends Model
+{
+    use HasFactory;
+    
+    protected $table = "staff_logs";
+
+    protected $fillable = [
+        'target_id',
+        'description',
+        'action_by_id',
+    ];
+
+    protected $casts = [
+        'created_at' => 'date:d/m/Y',
+        'updated_at' => 'date:d/m/Y',
+    ];
+
+    public function staff(): BelongsTo
+    {
+        return $this->belongsTo(Staff::class, 'target_id', 'id');
+    }
+
+    public function actionUser(): BelongsTo
+    {        
+        return $this->belongsTo(User::class, 'action_by_id', 'id');
+    }
+}

296fa3b287d8b68f0f94e11e225061b929c15b0b
Manh Huy Vo <manhhuyvo@gmail.com>
2024-03-02 16:29:30 +1100

    Add migrations for user, customer, staff, supplier logs

---
+++ database/migrations/2024_03_02_122430_create_staff_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('target_id')->index();
+            $table->longText('description');
+            $table->unsignedBigInteger('action_by_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff_logs');
+    }
+};

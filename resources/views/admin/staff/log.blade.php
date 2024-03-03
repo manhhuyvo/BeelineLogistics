cc1cc93cde133887c0cf1853fd18046b777c7167
Manh Huy Vo <mhuyvo@nexigen.digital>
2024-03-03 13:10:35 +1100

    Basic sketch for staff logs page

---
+++ resources/views/admin/staff/log.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layout.layout')
+@section('content')
+
+<div class="relative sm:rounded-lg">
+    @include('admin.layout.response')
+    <h2 class="text-2xl font-medium mt-2 mb-3">All staff logs</h2>
+<div>
+@endsection

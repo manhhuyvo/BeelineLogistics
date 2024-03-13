86266276ef8cad8248d1f149d763562db0ca60a3
Manh Huy Vo <manhhuyvo@gmail.com>
2024-03-13 21:27:12 +1100

    Add details for transaction repo, payment repo, base payable repo, etc

--- app/Repositories/TransactionRepo.php
+++ app/Repositories/TransactionRepo.php
@@ -5,16 +5,151 @@ namespace App\Repositories;
 use App\Repositories\Base\BaseRepo;
 use App\Models\Transaction;
 use App\Enums\TransactionEnum;
+use App\Repositories\PaymentRepo;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class TransactionRepo extends BaseRepo
-{    
-    public function __construct(Transaction $model)
+{   
+    private PaymentRepo $paymentRepo;
+
+    public function __construct(Transaction $model, PaymentRepo $paymentRepo)
     {
         $this->model = $model;
+        $this->paymentRepo = $paymentRepo;
     }
 
     public function getFilterableColumns(): array
     {
         return TransactionEnum::FILTERABLE_COLUMNS;
+    }
+
+    public function findAllByTarget(array $columns = ['*'], array $relations = [], ?string $target): Collection
+    {
+        $extra = [
+            'where' => ['target', $target],
+        ];
+
+        return $this->findAll($columns, $relations, $extra);
+    }
+
+    public function findAllByTargetAndTargetId(array $columns = ['*'], array $relations = [], ?string $target, ?int $targetId): Collection
+    {
+        $extra = [
+            'where' => ['target', $target],
+            'where' => ['target_id', $targetId],
+        ];
+
+        return $this->findAll($columns, $relations, $extra);
+    }
+
+    public function findAllByTargetWithPagination(
+        array $filters = [],
+        array $columns = ['*'],
+        array $relations = [],
+        ?string $target,
+        array $appends = [],
+        array $paginationDetails = [50, 'page']
+    ): ?LengthAwarePaginator
+    {
+        $extra = [
+            'where' => ['target', $target],
+        ];
+
+        return $this->findAllWithPagination($filters, $columns, $relations, $extra, $appends, $paginationDetails);
+    }
+
+    public function findAllByTargetAndTargetIdWithPagination(
+        array $filters = [],
+        array $columns = ['*'],
+        array $relations = [],
+        ?string $target,
+        ?int $targetId,
+        array $appends = [],
+        array $paginationDetails = [50, 'page']
+    ): ?LengthAwarePaginator
+    {
+        $extra = [
+            'where' => ['target', $target],
+            'where' => ['target_id', $targetId],
+        ];
+
+        return $this->findAllWithPagination($filters, $columns, $relations, $extra, $appends, $paginationDetails);
+    }
+
+    public function addTransactionWithoutPayment(array $payload): ?Model
+    {
+        return $this->create($payload);
+    }
+
+    public function addTransactionWithPayment(Request $request, array $transactionPayload, array $paymentPayload): ?array
+    {
+        $transaction = $this->create($transactionPayload);
+
+        if (!$transaction) {
+            return null;
+        }
+
+        $paymentPayload = array_merge(['transaction_id' => $transaction->id], $paymentPayload);
+
+        $paymentReceiptPath = $this->paymentRepo->addPaymentReceipt($request, $transactionPayload['target'], $transactionPayload['target_id']);
+        $paymentPayload = array_merge($paymentPayload, ['payment_receipt' => $paymentReceiptPath]);
+
+        $payment = $this->paymentRepo->create($paymentPayload);
+
+        if (!$payment) {
+            return null;
+        }
+
+        return [
+            'transaction' => $transaction,
+            'payment' => $payment,
+        ];
+    }
+
+    public function approveTransaction(?int $id): bool
+    {
+        $model = $this->findById($id);
+
+        if (!$model->payment) {
+            return true;
+        }
+
+        return $this->paymentRepo->approvePayment($model->payment);
+    }
+
+    public function declineTransaction(?int $id): bool
+    {
+        $model = $this->findById($id);
+
+        if (!$model->payment) {
+            return true;
+        }
+
+        return $this->paymentRepo->declinePayment($model->payment);
+    }
+
+    public function pendTransaction(?int $id): bool
+    {
+        $model = $this->findById($id);
+
+        if (!$model->payment) {
+            return true;
+        }
+
+        return $this->paymentRepo->pendPayment($model->payment);
+    }
+
+    public function deleteTransaction(?int $id): bool
+    {
+        $model = $this->findById($id);
+
+        if (!$model->payment) {
+            return true;
+        }
+
+        return $this->paymentRepo->deletePayment($model->payment);
     }
 }
